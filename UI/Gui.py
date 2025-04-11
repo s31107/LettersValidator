82c7c91285d4550f196498b898a136254b2460e8
Jakub Beno <s31107@pjwstk.edu.pl>
2025-04-11 14:03:00 +0200

    Gui refactor

--- UI/Gui.py
+++ UI/Gui.py
@@ -1,44 +1,50 @@
-import io
-import tkinter as tk
-
-from PIL import Image, ImageOps
+import tkinter
+from PIL import Image, ImageDraw
 
 
-def get_binary_pixels(canvas: tk.Canvas):
-    image = Image.open(io.BytesIO(canvas.postscript(colormode="gray")))
-    resized_image = image.convert("L").resize((100, 100))
-    return [int(item == 0) for item in resized_image.getdata()]
+def get_picture(matrix_width: int, matrix_height: int) -> list[int]:
+    root = tkinter.Tk()
+    app = LetterDrawer(root)
+    root.mainloop()
+    return app.get_image(matrix_width, matrix_height)
+
 
 class LetterDrawer:
-    last_x, last_y = None, None
-    def __init__(self, root):
+    _last_x: float = None
+    _last_y: float = None
+    _canvas_size: tuple[int, int] = (500, 500, )
 
-        self.canvas = tk.Canvas(root, width=500, height=500, bg="white")
-        self.canvas.pack()
+    def __init__(self, root: tkinter.Tk):
+        self._canvas: tkinter.Canvas = tkinter.Canvas(
+            root, width=self._canvas_size[0], height=self._canvas_size[1], bg="white")
+        self._canvas.pack()
 
-        self.clear_button = tk.Button(root, text="Wyczyść", command=self.clear_canvas)
-        self.accept_button = tk.Button(root, text="Potwierdź")
-        self.accept_button.pack(side=tk.RIGHT, padx=5, pady=5)
-        self.clear_button.pack(side=tk.RIGHT, padx=5, pady=5)
+        clear_button: tkinter.Button = tkinter.Button(root, text="Wyczyść", command=self.clear_canvas)
+        accept_button: tkinter.Button = tkinter.Button(root, text="Potwierdź", command=root.destroy)
+        accept_button.pack(side=tkinter.RIGHT, padx=5, pady=5)
+        clear_button.pack(side=tkinter.RIGHT, padx=5, pady=5)
 
-        self.canvas.bind("<B1-Motion>", self.draw)
-        self.canvas.bind("<ButtonRelease-1>", lambda _: self.set_default_x_y())
+        self._canvas.bind("<B1-Motion>", self.draw)
+        self._canvas.bind("<ButtonRelease-1>", lambda _: self.set_default_x_y())
 
-    def set_default_x_y(self):
-        self.last_x, self.last_y = None, None
+        self._image = Image.new("L", self._canvas_size, color="white")
+        self._image_draw = ImageDraw.Draw(self._image)
 
-    def draw(self, event):
-        if self.last_x is not None and self.last_y is not None:
-            self.canvas.create_line(self.last_x, self.last_y, event.x, event.y,
-                                    width=6, fill='black', capstyle=tk.ROUND, smooth=True)
-        self.last_x = event.x
-        self.last_y = event.y
+    def set_default_x_y(self) -> None:
+        self._last_x: None = None
+        self._last_y: None = None
 
-    def clear_canvas(self):
-        self.canvas.delete("all")
+    def draw(self, event) -> None:
+        if self._last_x is not None and self._last_y is not None:
+            self._canvas.create_line(self._last_x, self._last_y, event.x, event.y,
+                                     width=6, fill='black', capstyle=tkinter.ROUND, smooth=True)
+            self._image_draw.line([self._last_x, self._last_y, event.x, event.y])
+        self._last_x: float = event.x
+        self._last_y: float = event.y
+
+    def clear_canvas(self) -> None:
+        self._canvas.delete("all")
         self.set_default_x_y()
 
-
-root = tk.Tk()
-app = LetterDrawer(root)
-root.mainloop()
+    def get_image(self, width: int, height: int) -> list[int]:
+        return [int(item == 0) for item in self._image.resize((width, height,), Image.NEAREST).getdata()]
